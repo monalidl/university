a07f1d8f8e3fe2f08f7e66fbb134944c53987962
Monali Lakhote <monalidl@gmail.com>
2020-01-25 12:10:55 -0500

    Edited preloaded db

--- src/main/java/com/learning/university/LoadDatabase.java
+++ src/main/java/com/learning/university/LoadDatabase.java
@@ -1,9 +1,14 @@
 package com.learning.university;
 
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.mapping.Collection;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author Monali L on 1/20/2020
@@ -16,21 +21,23 @@ public class LoadDatabase {
     @Bean
     CommandLineRunner initDatabase (StudentRepository studentRepo, SubjectRepository subjectRepo) {
 
-        studentRepo.save(new Student("Ron", "Potions"));
-        studentRepo.save(new Student("Harry", "Herbology"));
-        studentRepo.save(new Student("Hermione", "Transfiguration"));
-        studentRepo.save(new Student("Ginnie", "Defense against the Dark Arts"));
-        studentRepo.save(new Student("Luna", "Potions"));
-        studentRepo.save(new Student("Malfoy", "Herbology"));
+        //subjectRepo.save(new Subject("Defense against the Dark Arts", "Albus Dumbledore"));
+        subjectRepo.save(new Subject("Herbology", "Pomona Sprout"));
+        subjectRepo.save(new Subject("Transfiguration",  "Minerva McGonagall"));
+        subjectRepo.save(new Subject("Potions", "Horace Slughorn"));
 
-        subjectRepo.save(new Subject("Defense against the Dark Arts"));
-        subjectRepo.save(new Subject("Herbology"));
-        subjectRepo.save(new Subject("Transfiguration"));
-        subjectRepo.save(new Subject("Potions"));
+        studentRepo.save(new Student("Ron Weasley"));
+        studentRepo.save(new Student("Harry Potter"));
+        studentRepo.save(new Student("Hermoine Grenger"));
+        /*studentRepo.save(new Student("Harry", Arrays.asList(subjectRepo.getOne((long) 2))));
+        studentRepo.save(new Student("Hermione", Arrays.asList(subjectRepo.getOne((long) 4))));
+        studentRepo.save(new Student("Ginnie", Arrays.asList(subjectRepo.getOne((long) 3))));
+        studentRepo.save(new Student("Luna", Arrays.asList(subjectRepo.getOne((long) 1))));
+        studentRepo.save(new Student("Malfoy", Arrays.asList(subjectRepo.getOne((long) 2))));*/
 
         return args -> {
-            log.info("Preloading " + studentRepo.findAll());
-            log.info("Preloading " + subjectRepo.findAll());
+            log.info("Preloading " + studentRepo);
+            log.info("Preloading " + subjectRepo);
         };
     }
 }
